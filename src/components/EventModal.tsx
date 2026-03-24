c8f28e12452ab27d34fb996ca69925fb9fd4fc6f
Hevrutah <yaad.medan@hevrutah.co.il>
2026-03-24 21:26:21 +0200

    Fix TypeScript build: remove unused toInputValue function

--- src/components/EventModal.tsx
+++ src/components/EventModal.tsx
@@ -24,10 +24,6 @@ interface Props {
   onSaved: () => void;
 }
 
-function toInputValue(date: Date): string {
-  const pad = (n: number) => String(n).padStart(2, '0');
-  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`;
-}
 
 function toDateInputValue(date: Date): string {
   const pad = (n: number) => String(n).padStart(2, '0');
